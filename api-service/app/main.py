21121e897236f893d67b3a24269e3e717af52b76
Ajeetashu-14 <rohilla.ajeet12@gmail.com>
2026-01-15 00:10:35 +0530

    added worker service with some updated and added docker-compose file

--- api-service/app/main.py
+++ api-service/app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.routes import health, job_routes
+import os
+
+required_envs=["DATABASE_URL","REDIS_HOST"]
+
+for env in required_envs:
+    if not os.getenv(env):
+        raise RuntimeError(f"Missing required env var: {env}")
 
 app = FastAPI(title="FastAPI Platform")
 
